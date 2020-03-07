1170589238b7e59ff415c37b4ee42b42daa11a9d
Debora Frias <deehconstantino@gmail.com>
2020-03-06 23:03:26 -0300

    Adicionada validação para login incorreto

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,19 +1,23 @@
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { PoNotificationService } from '@portinari/portinari-ui';
 
 @Injectable()
 export class LoginService {
     private loggedIn = new BehaviorSubject<boolean>(false);
 
-    constructor(private router: Router) {}
+    constructor(private router: Router,
+        private poNotification: PoNotificationService) {}
 
     doLogin(user) {
         if (user.login === "admin" && user.password === "admin") {
             this.loggedIn.next(true);
             this.router.navigate(['tasks']);
+        } else {
+            this.poNotification.error("Login inválido! Tente novamente.")
+            return false;
         }
-        return false;
     }
 
     get isLoggedIn() {
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,33 +1,41 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { DatePipe } from '@angular/common';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import 'rxjs/add/operator/switchMap';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { DatePipe } from "@angular/common";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import "rxjs/add/operator/switchMap";
 
-import { PoTableColumn, PoTableAction, PoModalComponent, PoNotificationService, PoDialogService, PoRadioGroupOption, PoCheckboxGroupOption } from '@portinari/portinari-ui';
+import {
+  PoTableColumn,
+  PoTableAction,
+  PoModalComponent,
+  PoNotificationService,
+  PoDialogService,
+  PoRadioGroupOption,
+} from "@portinari/portinari-ui";
 
-import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { TasksService } from "./tasks.service";
+import { Task } from "./task.model";
 
 @Component({
-  selector: 'sample-po-page-dynamic-table-users',
-  templateUrl: './tasks.component.html',
+  selector: "sample-po-page-dynamic-table-users",
+  templateUrl: "./tasks.component.html",
   preserveWhitespaces: true
 })
-
 export class TasksComponent implements OnInit {
   task: Task;
 
-  searchForm: FormGroup
-  searchControl: FormControl
+  searchForm: FormGroup;
+  searchControl: FormControl;
 
-  constructor(private tasksService: TasksService,
+  constructor(
+    private tasksService: TasksService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private poNotification: PoNotificationService,
     private poAlert: PoDialogService,
     private datePipe: DatePipe,
-    private formBuilder: FormBuilder) { }
+    private formBuilder: FormBuilder
+  ) {}
 
   columns: Array<PoTableColumn>;
   items: any;
@@ -37,28 +45,45 @@ export class TasksComponent implements OnInit {
   date = new Date();
 
   readonly options: Array<PoRadioGroupOption> = [
-    { label: 'Tarefa', value: 'task' },
-    { label: 'Categoria', value: 'category' },
-    { label: 'Data', value: 'date' }
+    { label: "Tudo", value: "all" },
+    { label: "Tarefa", value: "task" },
+    { label: "Categoria", value: "category" },
+    { label: "Data", value: "date" }
   ];
 
   ngOnInit() {
     this.columns = this.tasksService.getColumns();
-    this.tasksService.getTasks()
-    .subscribe(dados => this.items = dados
-      .map((dado) => ({... dado,
-      status: this.tasksService.updStatus(dado.start, dado.end, dado.status)
-    })))
+    this.tasksService
+      .getTasks()
+      .subscribe(
+        dados =>
+          (this.items = dados.map(dado => ({
+            ...dado,
+            status: this.tasksService.updStatus(
+              dado.start,
+              dado.end,
+              dado.status
+            )
+          })))
+      );
 
-    this.searchControl = this.formBuilder.control('')
+    this.searchControl = this.formBuilder.control("");
     this.searchForm = this.formBuilder.group({
       searchControl: this.searchControl
-    })
+    });
   }
 
   actions: Array<PoTableAction> = [
-    { action: this.viewTask.bind(this), icon: 'po-icon po-icon-eye', label: 'Detalhes' },
-    { action: this.openDialogEnd.bind(this), icon: 'po-icon po-icon-ok', label: 'Finalizar' }
+    {
+      action: this.viewTask.bind(this),
+      icon: "po-icon po-icon-eye",
+      label: "Detalhes"
+    },
+    {
+      action: this.openDialogEnd.bind(this),
+      icon: "po-icon po-icon-ok",
+      label: "Finalizar"
+    }
   ];
 
   @ViewChild(PoModalComponent, { static: true }) poModal: PoModalComponent;
@@ -69,15 +94,18 @@ export class TasksComponent implements OnInit {
   }
 
   updateTask(task) {
-    this.detail = task
+    this.detail = task;
     if (this.detail.done == true) {
-      this.poNotification.error("Essa tarefa já foi finalizada.")
+      this.poNotification.error("Essa tarefa já foi finalizada.");
     } else {
-      this.detail.end = this.datePipe.transform(this.date, 'yyyy-MM-dd')
-      this.detail.done = true
-      this.detail.status = "finished"
-      this.tasksService.updateTask(this.detail)
-      .subscribe(a => this.poNotification.success("Tarefa finalizada com sucesso!"));
+      this.detail.end = this.datePipe.transform(this.date, "yyyy-MM-dd");
+      this.detail.done = true;
+      this.detail.status = "finished";
+      this.tasksService
+        .updateTask(this.detail)
+        .subscribe(a =>
+          this.poNotification.success("Tarefa finalizada com sucesso!")
+        );
       this.refresh();
     }
   }
@@ -85,7 +113,8 @@ export class TasksComponent implements OnInit {
   openDialogEnd(id) {
     this.poAlert.confirm({
       title: "Finalizar tarefa",
-      message: "Confirma a conclusão da tarefa? Esta ação não poderá ser desfeita.",
+      message:
+        "Confirma a conclusão da tarefa? Esta ação não poderá ser desfeita.",
       confirm: () => this.updateTask(id),
       cancel: () => this.refresh()
     });
@@ -100,46 +129,93 @@ export class TasksComponent implements OnInit {
     });
   }
 
-  edit(id){
-    this.router.navigate(['edit', id], { relativeTo: this.activatedRoute });
+  edit(id) {
+    this.router.navigate(["edit", id], { relativeTo: this.activatedRoute });
   }
 
   remove(id) {
-    this.tasksService.removeTask(id)
-    .subscribe(a => {
+    this.tasksService.removeTask(id).subscribe(a => {
       this.poNotification.success("Tarefa excluída com sucesso!"),
-      this.poModal.close();
+        this.poModal.close();
       this.refresh();
     });
   }
 
   refresh() {
-    this.tasksService.getTasks().subscribe(dados => this.items = dados
-    .map((dado) => ({... dado, status: this.tasksService.updStatus(dado.start, dado.end, dado.status)})))
+    this.tasksService
+      .getTasks()
+      .subscribe(
+        dados =>
+          (this.items = dados.map(dado => ({
+            ...dado,
+            status: this.tasksService.updStatus(
+              dado.start,
+              dado.end,
+              dado.status
+            )
+          })))
+      );
   }
 
   change(value) {
     if (value == "category") {
-      this.searchControl.valueChanges.switchMap(searchTerm => 
-        this.tasksService.getTasksByCategory(searchTerm))
-        .subscribe(dados => this.items = dados
-          .map((dado) => ({... dado,
-            status: this.tasksService.updStatus(dado.start, dado.end, dado.status)
-          })))
+      this.searchControl.valueChanges
+        .switchMap(searchTerm =>
+          this.tasksService.getTasksByCategory(searchTerm)
+        )
+        .subscribe(
+          dados =>
+            (this.items = dados.map(dado => ({
+              ...dado,
+              status: this.tasksService.updStatus(
+                dado.start,
+                dado.end,
+                dado.status
+              )
+            })))
+        );
     } else if (value == "task") {
-      this.searchControl.valueChanges.switchMap(searchTerm => 
-        this.tasksService.getTasksByName(searchTerm))
-        .subscribe(dados => this.items = dados
-          .map((dado) => ({... dado,
-            status: this.tasksService.updStatus(dado.start, dado.end, dado.status)
-          })))
+      this.searchControl.valueChanges
+        .switchMap(searchTerm => this.tasksService.getTasksByName(searchTerm))
+        .subscribe(
+          dados =>
+            (this.items = dados.map(dado => ({
+              ...dado,
+              status: this.tasksService.updStatus(
+                dado.start,
+                dado.end,
+                dado.status
+              )
+            })))
+        );
+    } else if (value == "date") {
+      this.searchControl.valueChanges
+        .switchMap(searchTerm => this.tasksService.getTasksByDate(searchTerm))
+        .subscribe(
+          dados =>
+            (this.items = dados.map(dado => ({
+              ...dado,
+              status: this.tasksService.updStatus(
+                dado.start,
+                dado.end,
+                dado.status
+              )
+            })))
+        );
     } else {
-      this.searchControl.valueChanges.switchMap(searchTerm => 
-        this.tasksService.getTasksByDate(searchTerm))
-        .subscribe(dados => this.items = dados
-          .map((dado) => ({... dado,
-            status: this.tasksService.updStatus(dado.start, dado.end, dado.status)
-          })))
+      this.tasksService
+        .getTasks()
+        .subscribe(
+          dados =>
+            (this.items = dados.map(dado => ({
+              ...dado,
+              status: this.tasksService.updStatus(
+                dado.start,
+                dado.end,
+                dado.status
+              )
+            })))
+        );
     }
   }
 }
