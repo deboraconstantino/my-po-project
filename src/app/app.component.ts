d745e9222ee0cd3b601945601064b1e9b361f7c8
Debora Frias <deehconstantino@gmail.com>
2020-03-08 20:06:36 -0300

    Adicionados submenus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { PoMenuItem } from '@portinari/portinari-ui';
-import { TasksService } from './tasks/tasks.service';
-import { Observable } from 'rxjs';
-import { LoginService } from './login/login.service';
-import { CategoriesService } from './categories/categories.service';
+import { PoMenuItem } from "@portinari/portinari-ui";
+import { TasksService } from "./tasks/tasks.service";
+import { Observable } from "rxjs";
+import { LoginService } from "./login/login.service";
+import { CategoriesService } from "./categories/categories.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
   authOk$: Observable<boolean>;
 
-  constructor(private tasksService: TasksService,
+  constructor(
+    private tasksService: TasksService,
     private loginService: LoginService,
-    private categoriesService: CategoriesService) {}
+    private categoriesService: CategoriesService
+  ) {}
 
   ngOnInit(): void {
-    this.authOk$ = this.loginService.isLoggedIn
+    this.authOk$ = this.loginService.isLoggedIn;
   }
 
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Minhas Tarefas', link: 'tasks', action: () => this.tasksService.setStatus("false") },
-    { label: 'Tarefas Finalizadas', link: 'end-tasks', action: () => this.tasksService.setStatus("true") },
-    { label: 'Incluir Tarefas', link: 'form-tasks' },
-    { label: 'Categorias', link: 'categories', action: () => this.categoriesService.getCategories() },
-    { label: 'Logout', link: '/' }
+    {
+      label: "Minhas Tarefas",
+      subItems: [
+        {
+          label: "Atividades Pendentes",
+          action: () => this.tasksService.setStatus("false"),
+          link: "tasks"
+        },
+        {
+          label: "Tarefas Finalizadas",
+          link: "end-tasks",
+          action: () => this.tasksService.setStatus("true")
+        },
+        { label: "Incluir Tarefas",
+          link: "form-tasks"
+        }
+      ]
+    },
+    {
+      label: "Minhas Categorias",
+      subItems: [
+        { label: "Categorias",
+          link: "categories",
+          action: () => this.categoriesService.getCategories()
+        }
+      ]
+    },
+    { label: "Logout", link: "/" }
   ];
 }
